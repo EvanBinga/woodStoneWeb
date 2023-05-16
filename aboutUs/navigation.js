1bbbdf29d25849ae2f40946af168c64c9efced42
EvanBinga <106447704+EvanBinga@users.noreply.github.com>
2023-05-16 17:37:16 +0300

    1
    1

---
+++ aboutUs/navigation.js
@@ -0,0 +1,14 @@
+var burgerButtons = document.getElementsByClassName('menu-btn');
+
+// Добавляем обработчик события к каждой кнопке бургер-меню
+for (var i = 0; i < burgerButtons.length; i++) {
+  burgerButtons[i].addEventListener('click', toggleBurgerMenu);
+}
+
+function toggleBurgerMenu() {
+  // Получаем ссылку на элемент бургер-меню
+  var burgerMenu = document.getElementsByClassName('navigation')[0];
+
+  // Добавляем/удаляем класс "active" у элемента меню
+  burgerMenu.classList.toggle('active');
+}
